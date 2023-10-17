1ebf9cdffce3188f32f5d80923a7cb63145a8790
SurrealEmil <emil.ejderklev1@hotmail.com>
2023-10-17 22:38:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ÖvningGenericsClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tester = new TestGenerice();
+
+            int int1 = 6;
+            int int2 = 6;
+            bool intResult = tester.Compare(int1, int2);
+
+            Console.WriteLine($"Är de två talen lika? Resultatet är: {intResult}");
+
+            string string1 = "Hello";
+            string string2 = "world";
+            bool stringResultat = tester.Compare(string1, string2);
+
+            Console.WriteLine($"Är de två strängarna lika? Resultatet är: {stringResultat}");
+        }
+    }
+}
---
+++ TestGenerice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖvningGenericsClass
+{
+    internal class TestGenerice
+    {
+        public bool Compare<Thing>(Thing t1, Thing t2)
+        {
+            return t1.Equals(t2);
+        }
+    }
+}
